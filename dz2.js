0403185289b08a985f87c453bf0992227700ab2f
Angelkat2022 <katrinaprivat@gmail.com>
2023-05-02 12:16:59 +0300

    hw done

---
+++ dz2.js
@@ -0,0 +1,27 @@
+// Описание задания.
+
+//  1 уровень сложности: 1. Создать веб-приложение (Javascript). 
+
+
+// Создать Dockerfile. Скопировать в контейнер необходимые файлы и установить зависимости. 
+
+// Создать новый образ js-app:1.0.
+
+// Запустить контейнер с пробросом внутреннего TCP порта на host порт 8000.
+
+// Проверить работу веб-приложения в браузере:
+// http://localhost:8000 
+
+
+// Пример проекта NodeJS - Redis:
+// https://github.com/docker/awesome-compose/tree/master/nginx-nodejs-redis
+
+
+// Пример NodeJS сервера:
+// https://github.com/docker/awesome-compose/blob/master/nginx-nodejs-redis/web/server.js
+
+
+// Пример Dockerfile:
+// https://github.com/docker/awesome-compose/blob/master/nginx-nodejs-redis/web/Dockerfile
+
+// Done!
